0686d309b3210ade43c029e057b72c04155c2730
Martin <martin.benedetto07@gmail.com>
2025-06-07 13:40:59 -0300

    * Creo page de viajes * Creo IconButton * Creo ViajeCard * Modifico Ruta de viajes/:id * Modifico variables de colores * "Mokeo" peticion de viajes * Agrego paginado

--- src/pages/Landing/ActionCardsSection/ActionCardsSection.jsx
+++ src/pages/Landing/ActionCardsSection/ActionCardsSection.jsx
@@ -18,7 +18,7 @@ const ActionCardsSection = () => {
                 <div className="action-card action-card-2">
                     <h2>Encontrá Viajes Ideales</h2>
                     <p>Buscar viajes que se ajusten a tu tipo de camión y disponibilidad. Oportunidades al alcance de un clic.</p>
-                    <Link to="/signup" className="action-button">
+                    <Link to="/viajes" className="action-button">
                         Buscar
                         <ArrowRightAltIcon className='arrow' />
                     </Link>
--- src/pages/Viajes/Viajes.jsx
+++ src/pages/Viajes/Viajes.jsx
@@ -1,6 +1,67 @@
+import './viajes.scss'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import FilterAltIcon from '@mui/icons-material/FilterAlt'
+import IconButton from "../../components/common/IconButton/IconButton"
+import ViajeCard from '../../components/common/ViajeCard/ViajeCard'
+import { useEffect, useState } from 'react'
+import { getViajes } from '../../api/services/viajesService'
+import { Pagination } from '@mui/material'
+
+const LIMIT = 8
+
 const Viajes = () => {
+    const [viajesPorPagina, setViajesPorPagina] = useState({})
+    const [viajes, setViajes] = useState([])
+    const [page, setPage] = useState(1)
+    const [totalViajes, setTotalViajes] = useState(0)
+
+    const fetchViajes = async (pagina) => {
+        if (viajesPorPagina[pagina]) {
+            setViajes(viajesPorPagina[pagina])
+            return
+        }
+
+        const offset = (pagina - 1) * LIMIT
+        const response = await getViajes(LIMIT, offset)
+
+        setViajesPorPagina(prev => ({
+            ...prev,
+            [pagina]: response.data
+        }))
+
+        setViajes(response.data)
+        setTotalViajes(response.total)
+    }
+
+    useEffect(() => {
+        fetchViajes(page)
+    }, [page])
+
+    const handleChange = (_, value) => {
+        setPage(value)
+        window.scrollTo({ top: 0 })
+    }
+
     return (
-        <div>Viajes</div>
+        <main id='viajes' className="container">
+            <h2>Viajes disponibles</h2>
+            <div className="filterSortControls">
+                <IconButton Icon={FilterListIcon}>Ordenar por</IconButton>
+                <IconButton Icon={FilterAltIcon}>Filtrar</IconButton>
+            </div>
+            <div className="viajes-container">
+                {Array.isArray(viajes) &&
+                    viajes.map(viaje => <ViajeCard key={viaje.id} viaje={viaje} />)}
+            </div>
+            <div className="pagination-container">
+                <Pagination
+                    count={Math.ceil(totalViajes / LIMIT)}
+                    page={page}
+                    onChange={handleChange}
+                    size="large"
+                />
+            </div>
+        </main>
     )
 }
 
